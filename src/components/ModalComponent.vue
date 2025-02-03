8f7551bb7ddcfe7c709373795a963aa61d1ff468
shahriar saleque <shahriar.saleque@tekarsh.com>
2025-02-03 20:03:51 +0600

    refactor to a common modal component

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,169 @@
+<script lang="ts">
+export default {
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+    },
+    peopleCount: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit('update:showModal', false)
+    },
+  },
+}
+</script>
+
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="modal-header-content">How many people?</div>
+        <div class="modal-header-cross-icon" @click="closeModal">
+          <i class="fas fa-times"></i>
+        </div>
+      </div>
+
+      <div class="horizontal-rule">
+        <hr />
+      </div>
+
+      <div class="modal-input">
+        <div class="modal-input-label">
+          <p>Enter the number of how many people you want to add to the list.</p>
+        </div>
+
+        <!-- <div class="modal-input-field">
+            <input type="number" v-model="peopleCount" />
+          </div> -->
+      </div>
+
+      <div class="horizontal-rule">
+        <hr />
+      </div>
+    </div>
+
+    <div class="modal-buttons">
+      <div class="cancel-btn">
+        <button @click="closeModal">Cancel</button>
+      </div>
+
+      <div class="start-btn">
+        <button @click="startGame">Start</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  width: 437px;
+  height: 282px;
+  top: 22px;
+  gap: 0px;
+  border-radius: 5px 0px 0px 0px;
+  background-color: white;
+  position: relative;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 10px;
+}
+
+.horizontal-rule {
+  hr {
+    width: 100%;
+    border: 1px solid #cccccc;
+  }
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+  .modal-header-content {
+    font-size: 20px;
+    font-weight: 600;
+    line-height: 27px;
+  }
+
+  .modal-header-cross-icon {
+    cursor: pointer;
+    width: 12px;
+    height: 12px;
+    position: relative;
+    top: 8px;
+    color: #999999;
+  }
+}
+
+.modal-input {
+  margin-top: 20px;
+  margin-bottom: 50px;
+
+  .modal-input-label {
+    p {
+      font-size: 14px;
+      margin-left: 10px;
+      font-weight: 400;
+      line-height: 22px;
+      color: #555555;
+    }
+  }
+
+  .modal-input-field {
+    input {
+      width: 400px;
+      height: 40px;
+      border: 1px solid #cccccc;
+      border-radius: 5px;
+      margin-top: 10px;
+      padding-left: 14px;
+      margin-left: 10px;
+    }
+  }
+}
+
+.modal-buttons {
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+}
+
+.start-btn {
+  button {
+    cursor: pointer;
+    width: 78px;
+    height: 40px;
+    background-color: #f68d03;
+    color: white;
+    border-radius: 10px;
+    border-color: transparent;
+    font-size: 14px;
+    line-height: 18px;
+    box-shadow: 0px 1.5px 1.5px rgba(0, 0, 0, 0.25);
+  }
+}
+
+.cancel-btn {
+  button {
+    cursor: pointer;
+    width: 88px;
+    height: 40px;
+    background-color: #eeeeee;
+    color: #555555;
+    border-radius: 10px;
+    border-color: transparent;
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 700;
+    box-shadow: 0px 1.5px 1.5px rgba(0, 0, 0, 0.25);
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,55 +1,46 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
+import ModalComponent from '@/components/ModalComponent.vue'
+
 const state = reactive({
-  showModal: true,
+  showModal: false,
   peopleCount: 20,
 })
 </script>
 
 <template>
-  <div v-if="state.showModal" class="modal-overlay">
-    <div class="modal">
-      <div class="modal-content">
-        <div class="modal-header">
-          <div class="modal-header-content">How many people?</div>
-          <div class="modal-header-cross-icon" @click="state.showModal = false">
-            <i class="fas fa-times"></i>
-          </div>
+  <div id="app">
+    <div class="table">
+      <div class="table-header">
+        <div class="table-header-content">
+          <p>Sorting Training System</p>
         </div>
-
-        <div class="horizontal-rule">
-          <hr />
-        </div>
-
-        <div class="modal-input">
-          <div class="modal-input-label">
-            <p>Enter the number of how many people you want to add to the list.</p>
+        <div class="table-header-content">
+          <div class="table-header-content-button">
+            <button @click="state.showModal = true">Start sorting!</button>
           </div>
-
-          <div class="modal-input-field">
-            <input type="number" v-model="state.peopleCount" />
-          </div>
-        </div>
-
-        <div class="horizontal-rule">
-          <hr />
         </div>
       </div>
-
-      <div class="modal-buttons">
-        <div class="cancel-btn">
-          <button @click="state.showModal = false">Cancel</button>
-        </div>
-
-        <div class="start-btn" @click="startGame">
-          <button @click="startGame">Start</button>
-        </div>
-      </div>
+    </div>
+    <div v-if="state.showModal" class="modal-overlay">
+      <ModalComponent
+        :showModal="state.showModal"
+        :peopleCount="state.peopleCount"
+        @update:showModal="state.showModal = $event"
+      />
     </div>
   </div>
 </template>
 
 <style scoped>
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+}
+
 .modal-overlay {
   position: fixed;
   top: 0;
@@ -62,106 +53,39 @@ const state = reactive({
   align-items: center;
 }
 
-.modal {
-  font-family: 'Roboto', sans-serif;
-  width: 437px;
-  height: 282px;
-  top: 22px;
-  gap: 0px;
-  border-radius: 5px 0px 0px 0px;
-  background-color: white;
-  position: relative;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 10px;
-}
+.table {
+  font-family: Roboto;
 
-.horizontal-rule {
-  hr {
-    width: 100%;
-    border: 1px solid #cccccc;
-  }
-}
-.modal-header {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
+  .table-header {
+    display: flex;
+    justify-content: space-between;
 
-  .modal-header-content {
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 27px;
-  }
-
-  .modal-header-cross-icon {
-    cursor: pointer;
-    width: 12px;
-    height: 12px;
-    position: relative;
-    top: 8px;
-    color: #999999;
-  }
-}
-
-.modal-input {
-  margin-top: 20px;
-  margin-bottom: 50px;
-
-  .modal-input-label {
-    p {
-      font-size: 14px;
-      margin-left: 10px;
-      font-weight: 400;
-      line-height: 22px;
-      color: #555555;
+    .table-header-content {
+      p {
+        font-family: 'Roboto', sans-serif;
+        font-size: 25px;
+        font-weight: 700;
+        line-height: 37px;
+        text-align: left;
+      }
     }
-  }
 
-  .modal-input-field {
-    input {
-      width: 400px;
-      height: 40px;
-      border: 1px solid #cccccc;
-      border-radius: 5px;
-      margin-top: 10px;
-      padding-left: 14px;
-      margin-left: 10px;
+    .table-header-content-button {
+      button {
+        font-family: 'Roboto', sans-serif;
+        cursor: pointer;
+        border-radius: 10px;
+        background-color: #ff8d00;
+        border-color: transparent;
+        color: white;
+        font-size: 16px;
+        line-height: 20px;
+        box-shadow: 0px 1.5px 1.5px rgba(0, 0, 0, 0.25);
+        padding: 12px;
+        position: relative;
+        top: 20px;
+      }
     }
-  }
-}
-
-.modal-buttons {
-  display: flex;
-  justify-content: flex-end;
-  gap: 10px;
-}
-
-.start-btn {
-  button {
-    width: 78px;
-    height: 40px;
-    background-color: #f68d03;
-    color: white;
-    border-radius: 10px;
-    border-color: transparent;
-    font-size: 14px;
-    line-height: 18px;
-  }
-}
-
-.cancel-btn {
-  button {
-    width: 88px;
-    height: 40px;
-    background-color: #eeeeee;
-    color: #555555;
-    border-radius: 10px;
-    border-color: transparent;
-    font-size: 14px;
-    line-height: 18px;
-    font-weight: 700;
   }
 }
 </style>
